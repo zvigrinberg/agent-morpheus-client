3498c234eeeaa7e127c769879f5790ea23ae8b75
Zvi Grinberg <zgrinber@redhat.com>
2024-08-11 14:53:38 +0300

    style : reformat all python code style according to PEP8
    Signed-off-by: Zvi Grinberg <zgrinber@redhat.com>

--- callback/http_callback.py
+++ callback/http_callback.py
@@ -4,24 +4,25 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
 class HttpCallback:
 
-  def logging_callback(self, data):
-    logger.info('Received %s', data)
+    def logging_callback(self, data):
+        logger.info('Received %s', data)
 
-  def __init__(self):
-    self.api = Flask(__name__)
-    self.api.add_url_rule('/results', 'result_callback', self.result_callback, methods=['POST'])
-    self.port = os.getenv('CALLBACK_PORT', 5000)
-    self.on_receive = self.logging_callback
+    def __init__(self):
+        self.api = Flask(__name__)
+        self.api.add_url_rule('/results', 'result_callback', self.result_callback, methods=['POST'])
+        self.port = os.getenv('CALLBACK_PORT', 5000)
+        self.on_receive = self.logging_callback
 
-  def result_callback(self):
-    logger.info('Received request')
-    data = request.get_json()
-    self.on_receive(data)
-    return "Success", 201
+    def result_callback(self):
+        logger.info('Received request')
+        data = request.get_json()
+        self.on_receive(data)
+        return "Success", 201
 
-  def serve(self, on_receive):
-    if on_receive is not None:
-      self.on_receive = on_receive
-    self.api.run(host='0.0.0.0', port=self.port)
+    def serve(self, on_receive):
+        if on_receive is not None:
+            self.on_receive = on_receive
+        self.api.run(host='0.0.0.0', port=self.port)
--- utils/client_model.py
+++ utils/client_model.py
@@ -3,30 +3,36 @@ from pydantic import BaseModel, Field
 
 SUPPORTED_LANGUAGES = ['Go', 'Python', 'Dockerfile', 'Java', 'TypeScript', 'JavaScript']
 
+
 class SourceInfo(BaseModel):
-  type: str
-  git_repo: str
-  commit_id: str
-  include: list[str]
-  exclude: list[str] | None = None
+    type: str
+    git_repo: str
+    commit_id: str
+    include: list[str]
+    exclude: list[str] | None = None
+
 
 class SbomInfo(BaseModel):
-  type: str = Field(serialization_alias='_type')
-  format: str
-  content: dict
+    type: str = Field(serialization_alias='_type')
+    format: str
+    content: dict
+
 
 class Image(BaseModel):
-  name: str
-  tag: str
-  source_info: list[SourceInfo]
-  sbom_info: SbomInfo
+    name: str
+    tag: str
+    source_info: list[SourceInfo]
+    sbom_info: SbomInfo
+
 
 class Vuln(BaseModel):
-  vuln_id: str
+    vuln_id: str
+
 
 class Scan(BaseModel):
-  vulns: list[Vuln]
+    vulns: list[Vuln]
+
 
 class InputRequest(BaseModel):
-  image: Image
-  scan: Scan
+    image: Image
+    scan: Scan
